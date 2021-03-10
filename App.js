e60261276d9f9f7e0200e9f410f9eecc2db7a82f
revanthkumaar <iamrevanthkumar@gmail.com>
2021-03-10 16:46:03 +0530

    sdfsdf
    sdf

--- App.js
+++ App.js
@@ -1,27 +1,26 @@
 import 'react-native-gesture-handler';
 import React, {useEffect, useState} from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { LoginScreen, HomeScreen, RegistrationScreen } from './src/screens'
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {LoginScreen, HomeScreen, RegistrationScreen} from './src/screens';
 
+export default function App() {
+  const [user, setUser] = useState(null);
 
-export default function App(){
-
-  const [user,setUser] = useState(null)
-
-
-
-  return(
-
+  return (
     <NavigationContainer>
-
-    <Stack.Navigator>
-
-
-
-    </Stack.Navigator>
-
+      <Stack.Navigator>
+        {user ? (
+          <Stack.screen name="Home">
+            {(props) => <HomeScreen {...props} extraData={user} />}
+          </Stack.screen>
+        ) : (
+          <>
+            <Stack.Screen name="Login" component={LoginScreen} />
+            <Stack.screen name="Registration" component={RegistrationScreen} />
+          </>
+        )}
+      </Stack.Navigator>
     </NavigationContainer>
-
-  )
+  );
 }
